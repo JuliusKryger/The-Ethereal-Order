a40041591e4ce7d1bef9d44ae867f8f319284ca6
cph-mn605 <cph-mn605@cphbusiness.dk>
2022-06-22 17:15:37 +0200

    mainmenu image

---
+++ Assets/MusicController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+
+        if(instance == null)
+        {
+            instance=this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
